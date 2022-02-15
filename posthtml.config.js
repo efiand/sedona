36a304ad2e9f3bbad370143e9bd576ff9464cbe8
Andrey Efimov <efiand@ya.ru>
2022-02-15 11:37:54 +0300

    Разнесение серверной части по модулям

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,9 +1,6 @@
-const nunjucks = require('nunjucks');
 const render = require('posthtml-render');
 const parser = require('posthtml-parser');
-const WithExtension = require('@allmarkedup/nunjucks-with');
-const Util = require('./source/lib/util.cjs');
-const { readFile } = require('fs').promises;
+const { ServerUtil } = require('./source/lib/index.cjs');
 
 const layoutsDir = `${__dirname}/source/layouts`;
 const isDev = process.env.NODE_ENV === 'development';
@@ -18,42 +15,13 @@ module.exports = () => ({
         errors: []
       };
 
-      const applyData = (fn) => eval(`(${fn.replace(/;\s*$/, '')})`)(data, Util);
+      // Перечитываем код логики проекта при каждом изменении
+      data = await ServerUtil.modifyData(data, `${layoutsDir}/main.js`);
 
-      try {
-        // Перечитываем код логики проекта при каждом изменении
-        const handleMainData = await readFile(`${layoutsDir}/main.js`, 'utf-8');
-        data = {
-          ...data,
-          ...applyData(handleMainData)
-        };
-      } catch (err) {
-        data.errors.push(err);
-      }
+      // Перечитываем код логики страницы при каждом изменении
+      data = await ServerUtil.modifyData(data, `${layoutsDir}/pages/${page}.js`);
 
-      try {
-        // Перечитываем код логики страницы при каждом изменении
-        const handlePageData = await readFile(`${layoutsDir}/pages/${page}.js`, 'utf-8');
-        data = {
-          ...data,
-          ...applyData(handlePageData)
-        };
-      } catch (err) {
-        data.errors.push(err);
-      }
-
-      const env = nunjucks.configure('source', { autoescape: false });
-
-      env.addExtension('WithExtension', new WithExtension());
-      env.addGlobal('getContext', function() {
-        return this.ctx;
-      });
-      env.addFilter('keys', Object.keys);
-      for (const utilName of Object.keys(Util)) {
-        env.addFilter(utilName, Util[utilName]);
-      }
-
-      return parser(nunjucks.renderString(render(tree), data));
+      return parser(ServerUtil.renderNjk(render(tree), data));
     })(),
     require('posthtml-w3c')(),
     require('htmlnano')({ collapseWhitespace: 'aggressive' })
--- source/lib/util.cjs
+++ source/lib/util.cjs
@@ -1,6 +1,6 @@
 // Утилиты в формате CommonJS для работы posthtml и webpack
 
-module.exports = {
+const Util = {
   setProp(obj = {}, key, value = null) {
     obj[key] = value;
     return obj;
@@ -17,5 +17,12 @@ module.exports = {
   numberize(str) {
     const numberized = str.replace(/\D/g, '');
     return str.slice(0, 1) === '+' ? `+${numberized}` : numberized;
+  }
+};
+
+module.exports = {
+  ...Util,
+  applyDataWithFn(fn, data = {}) {
+    return eval(`(${fn.replace(/;\s*$/, '')})`)(data, Util);
   }
 };
