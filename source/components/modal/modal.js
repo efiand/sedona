2a84cf1fde941edb4484a7f6d21c4472e18fed03
Andrey Efimov <efiand@ya.ru>
2022-03-11 13:42:49 +0300

    Оставляем фокус внутри модалки

--- source/components/modal/modal.js
+++ source/components/modal/modal.js
@@ -1,13 +1,28 @@
+const FOCUSABLES = [
+  'a[href]',
+  'button:not(:disabled)',
+  'input:not(:disabled)',
+  'textarea:not(:disabled)',
+  'select:not(:disabled)',
+  '[tabindex]:not([tabindex^="-"])'
+].join(', ');
+
 export default class Modal {
   constructor(el) {
     this._el = el;
     this._id = this._el.dataset.modal;
     this._opener = document.querySelector(`[data-modal=${this._id}]`);
     this._closer = this._el.querySelector('.modal-closer');
+    this._allFocusables = [];
 
     this._setListeners();
 
     this._el.classList.add('modal-ready');
+  }
+
+  get _firstFocusable() {
+    const [firstFocusable] = this._el.querySelectorAll(FOCUSABLES);
+    return firstFocusable;
   }
 
   _setListeners() {
@@ -15,6 +30,7 @@ export default class Modal {
     this._handleBtnClose = this._handleBtnClose.bind(this);
     this._handleOverlayClose = this._handleOverlayClose.bind(this);
     this._handleKeyClose = this._handleKeyClose.bind(this);
+    this._handleFocus = this._handleFocus.bind(this);
 
     this._opener.addEventListener('click', this._handleOpen);
     this._el.addEventListener('click', this._handleOverlayClose);
@@ -25,14 +41,25 @@ export default class Modal {
     document.documentElement.style.overflow = 'hidden';
     document.documentElement.style.marginRight = 'var(--scrollbar-width)';
     document.addEventListener('keydown', this._handleKeyClose);
+
+    this._focusables = document.querySelectorAll(FOCUSABLES);
+    for (const focusableEl of this._focusables) {
+      focusableEl.addEventListener('focus', this._handleFocus);
+    }
+
     this._el.classList.add('modal-opened');
   }
 
   _close() {
     this._el.classList.remove('modal-opened');
+
     document.removeEventListener('keydown', this._handleKeyClose);
     document.documentElement.style.marginRight = '0';
     document.documentElement.style.overflow = 'auto';
+
+    for (const focusableEl of this._focusables) {
+      focusableEl.removeEventListener('focus', this._handleFocus);
+    }
   }
 
   _handleOpen(evt) {
@@ -55,6 +82,12 @@ export default class Modal {
     if (evt.key === 'Esc' || evt.key === 'Escape') {
       evt.preventDefault();
       this._close();
+    }
+  }
+
+  _handleFocus() {
+    if (document.activeElement.closest('.modal') !== this._el) {
+      this._firstFocusable.focus();
     }
   }
 }
